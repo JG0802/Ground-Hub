858f7bbbf6ae2522a49b570046657d36b392caa3
이재균 <jamesgyun@gmail.com>
2025-03-13 10:42:02 +0900

    3/13 초기 커밋: 게시판 빼고 구현됨

---
+++ src/main/java/com/ce/bool_dozer/BoolDozerApplication.java
@@ -0,0 +1,13 @@
+package com.ce.bool_dozer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoolDozerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoolDozerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ce/bool_dozer/BoolDozerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ce.bool_dozer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoolDozerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
